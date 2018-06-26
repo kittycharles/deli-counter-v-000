0a9773335888715e9c46f8ebecceeb4126026d25
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-26 21:25:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+  line_status = "The line is currently:"
+   array.each_with_index do |name, place|
+     line_status += " #{place.to_i+1}. #{name}"
+  end
+  puts "#{line_status}"
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line"
+end
